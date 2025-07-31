912376111ec13f471bdbdeaeb61e805eb4c5d4ff
Abhay Singh Tomar <122097124+Abhay1930@users.noreply.github.com>
2025-07-31 18:07:12 +0530

    Time: 0 ms (100%), Space: 46.1 MB (23.02%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, mid;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (target > nums[mid])
+                l = mid + 1;
+            else if (target < nums[mid])
+                r = mid - 1;
+            else if (target == nums[mid])
+                return mid;
+        }
+        return -1;
+    }
+}
