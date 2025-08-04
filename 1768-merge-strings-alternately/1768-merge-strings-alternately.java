77871189cfa66cd95ce15555dad1ceb211ce8c50
Abhay Singh Tomar <122097124+Abhay1930@users.noreply.github.com>
2025-08-04 15:34:09 +0530

    Time: 0 ms (100%), Space: 42 MB (34.94%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int o = Math.max(m, n);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < o; i++) {
+            if (i < m) {
+                sb.append(word1.charAt(i));
+            }
+            if (i < n) {
+                sb.append(word2.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
