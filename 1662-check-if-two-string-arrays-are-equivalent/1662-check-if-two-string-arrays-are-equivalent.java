8922b86d6659e84d81e89d3f9c20d15e63c818bc
Abhay Singh Tomar <122097124+Abhay1930@users.noreply.github.com>
2025-08-08 00:15:47 +0530

    Time: 0 ms (100%), Space: 41.7 MB (49.15%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,47 +1,17 @@
+
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        // if(word1.length != word2.length){
-        //     return false;
-        // }
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
 
-        // for(int i =0 ;i<word1.length-1;i++){
-        //     sb1.append(word1[i]);
-            
-        // }
         for (String s : word1) {
             sb1.append(s);
         }
-        String resultBuilder1 = sb1.toString();
 
-        //  for(int i =0 ;i<word1.length-1;i++){
-        //     sb2.append(word2[i]);
-        // }
         for (String s : word2) {
             sb2.append(s);
         }
-        String resultBuilder2 = sb2.toString();
 
-        if(resultBuilder1.equals(resultBuilder2)) {return true;}
-        return false;
-
-
+        return sb1.toString().equals(sb2.toString());
     }
 }
-// class Solution {
-//     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-//         StringBuilder sb1 = new StringBuilder();
-//         StringBuilder sb2 = new StringBuilder();
-
-//         for (String s : word1) {
-//             sb1.append(s);
-//         }
-
-//         for (String s : word2) {
-//             sb2.append(s);
-//         }
-
-//         return sb1.toString().equals(sb2.toString());
-//     }
-// }
